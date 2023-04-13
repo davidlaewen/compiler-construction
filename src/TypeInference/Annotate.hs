545c37472398b3b2f455a4b5ba7fbfe765f5911d
David Läwen <d.laewen@student.science.ru.nl>
2023-04-13 16:20:04 +0200

    Create type annotation function

---
+++ src/TypeInference/Annotate.hs
@@ -0,0 +1,47 @@
+module TypeInference.Annotate where
+
+import TypeInference.Definition
+import qualified Syntax.TypeAST as T
+
+annotateProgram :: Subst -> T.Program UType -> T.Program UType
+annotateProgram s (T.Program varDecls funDecls) =
+  T.Program (annotateVarDecl s <$> varDecls) (annotateFunDecl s <$> funDecls)
+
+annotateVarDecl :: Subst -> T.VarDecl UType -> T.VarDecl UType
+annotateVarDecl s (T.VarDecl mTy name expr ty) =
+  T.VarDecl mTy name (annotateExpr s expr) (subst s ty)
+
+annotateFunDecl :: Subst -> T.FunDecl UType -> T.FunDecl UType
+annotateFunDecl s (T.FunDecl name params mTy varDecls stmts ty) =
+  T.FunDecl name params mTy (annotateVarDecl s <$> varDecls) (annotateStmt s <$> stmts) (subst s ty)
+
+annotateStmt :: Subst -> T.Stmt UType -> T.Stmt UType
+annotateStmt s (T.If condExpr thenStmts elseStmts) =
+  T.If (annotateExpr s condExpr) (annotateStmt s <$> thenStmts) (annotateStmt s <$> elseStmts)
+annotateStmt s (T.While condExpr loopStmts) =
+  T.While (annotateExpr s condExpr) (annotateStmt s <$> loopStmts)
+annotateStmt s (T.Assign varLookup expr) =
+  T.Assign varLookup (annotateExpr s expr)
+annotateStmt s (T.FunCall funName args) =
+  T.FunCall funName (annotateExpr s <$> args)
+annotateStmt s (T.Return mExpr) =
+  T.Return (annotateExpr s <$> mExpr)
+
+annotateExpr :: Subst -> T.Expr UType -> T.Expr UType
+annotateExpr s (T.Ident name ty) =
+  T.Ident name (subst s ty)
+annotateExpr _ (T.Int n Int) =
+  T.Int n Int
+annotateExpr _ (T.Int _ ty) = error $ "Integer has type " ++ show ty ++ " instead of Int"
+annotateExpr _ (T.Char c Char) =
+  T.Char c Char
+annotateExpr _ (T.Char _ ty) = error $ "Character has type " ++ show ty ++ " instead of Char"
+annotateExpr _ (T.Bool b Bool) =
+  T.Bool b Bool
+annotateExpr _ (T.Bool _ ty) = error $ "Boolean has type " ++ show ty ++ " instead of Bool"
+annotateExpr s (T.FunCallE funName args ty) =
+  T.FunCallE funName (annotateExpr s <$> args) (subst s ty)
+annotateExpr s (T.EmptyList ty) =
+  T.EmptyList (subst s ty)
+annotateExpr s (T.Tuple e1 e2 ty) =
+  T.Tuple (annotateExpr s e1) (annotateExpr s e2) (subst s ty)
