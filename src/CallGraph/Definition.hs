d5084a1939d2b2ea68f1052eeaeb03c19a8920b0
David Läwen <d.laewen@student.science.ru.nl>
2025-04-23 13:53:24 +0200

    Implement call graph generation

--- src/CallGraph/Definition.hs
+++ src/CallGraph/Definition.hs
@@ -1,7 +1,86 @@
-module CallGraph.Definition () where
+{-# LANGUAGE LambdaCase, OverloadedRecordDot #-}
+
+module CallGraph.Definition (
+  GraphGen, runGraphGen, insertDecl, nameToVertex, insertEdge
+) where
 
 import Data.Graph
-import qualified Syntax.TypeAST as T
+import Control.Monad.State
+import Control.Monad.Except
+import qualified Data.Map as M
+import qualified Data.Text as T
 
-programToGraph :: T.Program () () -> Graph
-programToGraph p = buildG (0,0) []
+import Syntax.TypeAST
+import qualified Data.Set as S
+
+
+
+type Name = T.Text
+type NameMap = M.Map Name Vertex
+type DeclMap = M.Map Vertex (FunDecl () ())
+type EdgeMap = M.Map Vertex (S.Set Vertex)
+type VertexBound = Vertex
+
+-- The input type of `stronglyConnComp` is `[(node, key, [key])]`, where we
+-- can obtain the keys from the domain of `edgeMap`/`declMap`, the nodes from
+-- `declMap`, and the key lists from `edgeMap` with `S.toList`.
+
+-- TODO: Error localisation (calls to undefined functions)
+
+-- Data structures
+data GraphGenState = GraphGenState {
+  nameMap :: NameMap,
+  declMap :: DeclMap,
+  edgeMap :: EdgeMap,
+  vtxBound :: VertexBound
+}
+
+-- Call graph generation monad
+type GraphGen = StateT GraphGenState (Except T.Text)
+
+runGraphGen :: GraphGen a -> Either T.Text a
+runGraphGen x = fst <$> runExcept (runStateT x $
+  GraphGenState M.empty M.empty M.empty 0)
+
+
+modifyNameMap :: (NameMap -> NameMap) -> GraphGen ()
+modifyNameMap f = modify (\s -> s{ nameMap = f s.nameMap })
+
+modifyEdgeMap :: (EdgeMap -> EdgeMap) -> GraphGen ()
+modifyEdgeMap f = modify (\s -> s{ edgeMap = f s.edgeMap })
+
+modifyDeclMap :: (DeclMap -> DeclMap) -> GraphGen ()
+modifyDeclMap f = modify (\s -> s{ declMap = f s.declMap })
+
+insertName :: Name -> Vertex -> GraphGen ()
+insertName name ty = modifyNameMap (M.insert name ty)
+
+lookupName :: Name -> GraphGen (Maybe Vertex)
+lookupName name = gets (M.lookup name . nameMap)
+
+lookupDecl :: Vertex -> GraphGen (Maybe (FunDecl () ()))
+lookupDecl vtx = gets (M.lookup vtx . declMap)
+
+insertDecl :: Vertex -> FunDecl () () -> GraphGen ()
+insertDecl vtx decl = modifyDeclMap (M.insert vtx decl)
+
+freshVertex :: GraphGen Vertex
+freshVertex = do
+  i <- gets vtxBound
+  modify (\s -> s{ vtxBound = i+1 })
+  pure i
+
+nameToVertex :: Name -> GraphGen Vertex
+nameToVertex name = do
+  lookupName name >>= \case
+    Just vtx -> pure vtx
+    Nothing -> do -- Get fresh vertex and update `nameMap`
+      vtx <- freshVertex
+      insertName name vtx
+      pure vtx
+
+-- | Insert edge from vertex for `f` to node with name `g`
+insertEdge :: Vertex -> Name -> GraphGen ()
+insertEdge f g = do
+  vtx <- nameToVertex g
+  modifyEdgeMap (M.update (Just . S.insert vtx) f)
---
+++ src/CallGraph/GraphGen.hs
@@ -0,0 +1,69 @@
+module CallGraph.GenGraph (programToGraph) where
+
+import CallGraph.Definition
+import Syntax.TypeAST
+
+import Data.Graph
+
+
+-- | Takes vertex for current decl and expression, adds edges for fun calls
+exprToGraph :: Vertex -> Expr () -> GraphGen ()
+exprToGraph declVtx (FunCallE _ funName args _) = do
+  case funName of -- Insert edge for function call
+    Name name -> insertEdge declVtx name
+    _ -> pure () -- Unless it's a built-in function
+  mapM_ (exprToGraph declVtx) args
+-- Check for function calls in left and right subterm
+exprToGraph declVtx (Tuple _ lhs rhs _) = do
+  exprToGraph declVtx lhs
+  exprToGraph declVtx rhs
+-- Trivial cases; nothing happens here
+exprToGraph _ Ident{} = pure ()
+exprToGraph _ Int{} = pure ()
+exprToGraph _ Bool{} = pure ()
+exprToGraph _ Char{} = pure ()
+exprToGraph _ EmptyList{} = pure ()
+
+stmtToGraph :: Vertex -> Stmt () -> GraphGen ()
+stmtToGraph declVtx (If loc condExpr thenStmts elseStmts) = do
+  exprToGraph declVtx condExpr
+  mapM_ (stmtToGraph declVtx) thenStmts
+  mapM_ (stmtToGraph declVtx) elseStmts
+
+stmtToGraph declVtx (While loc condExpr stmts) = do
+  exprToGraph declVtx condExpr
+  mapM_ (stmtToGraph declVtx) stmts
+
+stmtToGraph declVtx (Assign _ varLookup ty expr) =
+  exprToGraph declVtx expr
+
+stmtToGraph declVtx (FunCall loc funName args) = do
+  case funName of -- Again, insert edge only for user-defined functions
+    Name name -> insertEdge declVtx name
+    _ -> pure ()
+  mapM_ (exprToGraph declVtx) args
+
+stmtToGraph declVtx (Return _ mExpr) = do
+  mapM_ (exprToGraph declVtx) mExpr
+
+-- | Only to be called for var decls at the start of a function decl, not the
+-- global var declarations, which we don't include in the SCC analysis
+varDeclToGraph :: Vertex -> VarDecl () -> GraphGen ()
+varDeclToGraph declVtx (VarDecl loc mty varName expr ty) = do
+  exprToGraph declVtx expr
+
+funDeclToGraph :: FunDecl () () -> GraphGen ()
+funDeclToGraph funDecl@(FunDecl _ funName argNames mty varDecls stmts ty) = do
+  declVtx <- nameToVertex funName -- Get existing or new vertex ID
+  insertDecl declVtx funDecl -- Insert into fun decl map
+  mapM_ (varDeclToGraph declVtx) varDecls
+  mapM_ (stmtToGraph declVtx) stmts
+
+funMutDeclToGraph :: FunMutDecl () () -> GraphGen ()
+funMutDeclToGraph (SingleDecl funDecl) = funDeclToGraph funDecl
+funMutDeclToGraph (MutualDecls _ funDecls) = do
+  mapM_ funDeclToGraph funDecls
+
+programToGraph :: Program () () -> GraphGen ()
+programToGraph (Program varDecls funMutDecls) = do
+  mapM_ funMutDeclToGraph funMutDecls
