38a12f65bcfebd9200d81dbcc523e7c0c20e7b44
David Läwen <d.laewen@student.science.ru.nl>
2025-04-27 12:06:01 +0200

    Discuss copy semantics of boxing vs monomorphisation

--- run-test.sh
+++ run-test.sh
@@ -2,6 +2,6 @@
 
 stack build
 
-file=tests-codegen/codegen.spl;
+file=tests-codegen/tuple-passing.spl;
 stack run -- codegen $file > output.ssm;
 java -jar ./../ssm/ssm.jar $* --file output.ssm --cli;
