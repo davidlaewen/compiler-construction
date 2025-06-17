f24343c7add37767367badd62a7bfb98d41354df
David Läwen <d.laewen@student.science.ru.nl>
2025-06-17 12:19:23 +0200

    Fix aexp example

--- run-gui.sh
+++ run-gui.sh
@@ -4,4 +4,4 @@ stack build
 
 file=$1; # Take file path as command line arg
 stack run -- codegen $file > output.ssm;
-java -jar ./../ssm/ssm.jar --file output.ssm;
+java -jar ./../ssm/ssm.jar --file output.ssm --guidelay 5;
